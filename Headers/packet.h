0152d3429f7966c60042f39f6d0dacc884dc91f4
Alireza_ramez <111694085+Alireza867599@users.noreply.github.com>
2024-12-06 22:54:08 +0330

    Update packet.h

--- Headers/packet.h
+++ Headers/packet.h
@@ -3,57 +3,42 @@
 #include <iostream>
 using namespace std;
 
-
+#include "datalinkheader.h"
+#include "tcpheader.h"
 #include "globals.h"
 #include <vector>
 #include <string>
 
-class IPHeader
-{
-public:
-    UT::IPVersion ipVersion;
-    std::string sourceIP;
-    std::string destinationIP;
+// class IPHeader
+// {
+// public:
+//     UT::IPVersion ipVersion;
+//     std::string sourceIP;
+//     std::string destinationIP;
 
-    IPHeader(UT::IPVersion version, const std::string& src, const std::string& dest)
-        : ipVersion(version), sourceIP(src), destinationIP(dest) {}
-};
+//     IPHeader(UT::IPVersion version, const std::string& src, const std::string& dest)
+//         : ipVersion(version), sourceIP(src), destinationIP(dest) {}
+// };
 
-class TCPHeader
-{
-public:
-    int sourcePort;
-    int destinationPort;
 
-    TCPHeader(int srcPort, int destPort) : sourcePort(srcPort), destinationPort(destPort) {}
-};
+// class BGPHeader
+// {
+// public:
+//     std::string bgpInfo;
 
-class BGPHeader
-{
-public:
-    std::string bgpInfo;
+//     BGPHeader(const std::string& info) : bgpInfo(info) {}
+// };
 
-    BGPHeader(const std::string& info) : bgpInfo(info) {}
-};
 
-class DataLinkHeader
-{
-public:
-    std::string macSource;
-    std::string macDestination;
-
-    DataLinkHeader(const std::string& src, const std::string& dest)
-        : macSource(src), macDestination(dest) {}
-};
 
 class Packet
 {
 private:
     UT::PacketType packetType;
     UT::PacketControlType controlType;
-    IPHeader* ipHeader;
-    TCPHeader* tcpHeader;
-    BGPHeader* bgpHeader;
+    // IPHeader* ipHeader;
+    TcpHeader* tcpHeader;
+    // BGPHeader* bgpHeader;
     DataLinkHeader* dataLinkHeader;
     std::string payload;
     int sequenceNumber;
@@ -62,14 +47,14 @@ private:
     std::vector<std::string> path;
 
 public:
-    Packet(UT::PacketType type, IPHeader* ip, TCPHeader* tcp, DataLinkHeader* dataLink,
+    Packet(UT::PacketType type , TcpHeader* tcp, DataLinkHeader* dataLink,
            const std::string& dataPayload, int seqNum, int waitCycles, int total);
 
     // For control packets with BGP Header
-    Packet(UT::PacketControlType controlType, IPHeader* ip, BGPHeader* bgp, DataLinkHeader* dataLink,
+    Packet(UT::PacketControlType controlType, DataLinkHeader* dataLink,
            int seqNum, int waitCycles, int total)
-        : packetType(UT::PacketType::Control), controlType(controlType), ipHeader(ip),
-        tcpHeader(nullptr), bgpHeader(bgp), dataLinkHeader(dataLink), sequenceNumber(seqNum),
+        : packetType(UT::PacketType::Control), controlType(controlType),
+        tcpHeader(nullptr), dataLinkHeader(dataLink), sequenceNumber(seqNum),
         waitingCycles(waitCycles), totalCycles(total) {}
 
     ~Packet();
@@ -77,9 +62,9 @@ public:
     // Getters and Setters
     UT::PacketType getPacketType() const;
     UT::PacketControlType getControlType() const;
-    IPHeader* getIPHeader() const;
-    TCPHeader* getTCPHeader() const;
-    BGPHeader* getBGPHeader() const;
+    // IPHeader* getIPHeader() const;
+    TcpHeader* getTCPHeader() const;
+    // BGPHeader* getBGPHeader() const;
     DataLinkHeader* getDataLinkHeader() const;
     const std::string& getPayload() const;
     int getSequenceNumber() const;
