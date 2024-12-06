e43bd1944c5d5f4fe3fd3adb316d24423249588f
Alireza_ramez <111694085+Alireza867599@users.noreply.github.com>
2024-12-06 22:52:38 +0330

    Update packet.cpp

--- Sources/packet.cpp
+++ Sources/packet.cpp
@@ -1,16 +1,16 @@
 #include "packet.h"
 
 
-Packet::Packet(UT::PacketType type, IPHeader *ip, TCPHeader *tcp, DataLinkHeader *dataLink, const string &dataPayload, int seqNum, int waitCycles, int total)
-    : packetType(type), ipHeader(ip), tcpHeader(tcp), bgpHeader(nullptr),
+Packet::Packet(UT::PacketType type, /*IPHeader *ip,*/ TcpHeader *tcp, DataLinkHeader *dataLink, const string &dataPayload, int seqNum, int waitCycles, int total)
+    : packetType(type), /*ipHeader(ip),*/ tcpHeader(tcp), /*bgpHeader(nullptr),*/
     dataLinkHeader(dataLink), payload(dataPayload), sequenceNumber(seqNum),
     waitingCycles(waitCycles), totalCycles(total) {}
 
 Packet::~Packet()
 {
-    delete ipHeader;
+    // delete ipHeader;
     delete tcpHeader;
-    delete bgpHeader;
+    // delete bgpHeader;
     delete dataLinkHeader;
 }
 
@@ -18,11 +18,11 @@ UT::PacketType Packet::getPacketType() const { return packetType; }
 
 UT::PacketControlType Packet::getControlType() const { return controlType; }
 
-IPHeader *Packet::getIPHeader() const { return ipHeader; }
+// IPHeader *Packet::getIPHeader() const { return ipHeader; }
 
-TCPHeader *Packet::getTCPHeader() const { return tcpHeader; }
+TcpHeader *Packet::getTCPHeader() const { return tcpHeader; }
 
-BGPHeader *Packet::getBGPHeader() const { return bgpHeader; }
+// BGPHeader *Packet::getBGPHeader() const { return bgpHeader; }
 
 DataLinkHeader *Packet::getDataLinkHeader() const { return dataLinkHeader; }
 
