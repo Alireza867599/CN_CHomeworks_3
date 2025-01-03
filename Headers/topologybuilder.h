ab92163e98c229d350eed96872b65015587bb56f
Alireza_ramez <111694085+Alireza867599@users.noreply.github.com>
2025-01-03 15:37:18 +0330

    Update topologybuilder.h

--- Headers/topologybuilder.h
+++ Headers/topologybuilder.h
@@ -3,25 +3,40 @@
 
 #include <vector>
 #include <memory>
+#include <string>
+#include <unordered_map>
+#include "C:/Users/Entezar/Documents/CA3/include/json.hpp"
 #include "router.h"
 #include "pc.h"
-#include "port.h"
+#include "autonomoussystem.h"
+using json = nlohmann::json;
 
 class TopologyBuilder {
 public:
-    TopologyBuilder();
-    ~TopologyBuilder();
-
-    void buildMeshTopology(int numRouters, int numPCs);
-    void buildStarRingTopology(int numRouters, int numPCs);
-    void resetTopology();
+    void loadConfig(const std::string& configFilePath);
+    void buildTopology(const json& config);
+    void printTopology() const ;
+    void buildRouters(const json& asConfig, std::shared_ptr<Router> dhcpRouter);
+    void connectUserGateways(const json& asConfig);
+    void connectAsGateways(const json& asConfig);
+    void connectPCsToRouters(const json& asConfig);
+    void buildPCs(const json& asConfig, std::shared_ptr<Router> dhcpServer);
+    void buildRoutersAndPCs(const json& asConfig, std::shared_ptr<Router> dhcpRouter);
 
 private:
-    std::vector<std::shared_ptr<Router>> routers;
-    std::vector<std::shared_ptr<PC>> pcs;
+    std::shared_ptr<Router> getDhcpRouterForAs(int asId);
 
-    void connectNodes(std::shared_ptr<Node> node1, std::shared_ptr<Node> node2);
-    void setupPorts(std::shared_ptr<Router> router, int numPorts);
+    std::unordered_map<int, std::shared_ptr<Router>> routers;
+    std::unordered_map<int, std::shared_ptr<PC>> pcs;
+    std::unordered_map<int, std::vector<std::shared_ptr<Router>>> autonomousSystems;
+    void buildTopology1(const json& config);
+
+    void buildASTopology(const json& asConfig);
+
+    void handleBrokenRouters(const json& asConfig);
+
+
+
 };
 
-#endif // TOPOLOGYBUILDER_H
+#endif // TOPOLOGY_BUILDER_H
