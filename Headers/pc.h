14c812a1063fd75f1d8c237c8f5c68beb749e373
Alireza_ramez <111694085+Alireza867599@users.noreply.github.com>
2025-01-03 15:38:29 +0330

    Update pc.h

--- Headers/pc.h
+++ Headers/pc.h
@@ -11,19 +11,28 @@
 #include <unordered_map>
 #include <iostream>
 #include <QMutex>
+class Router;
 
 class PC : public Node {
 private:
-    DataGenerator dataGenerator;           // For generating packet payloads
+    DataGenerator dataGenerator; // Member without a default constructor
     std::string ipAddress;                 // The IP address of this PC
     std::vector<std::string> otherIPs;     // Known IPs of other network users
     int port;                              // Port to send data through
     std::unordered_map<int, std::unique_ptr<Packet>> sentPackets; // Track sent packets by sequence number
     QMutex mutex;                          // Mutex to ensure thread safety
+    std::vector<std::shared_ptr<Port>> m_ports; // Container to store port
+    int PCID;
+    std::string macaddress;                 // The IP address of this PC
 
 public:
     // Constructor
-    PC(const std::string& ip, int port, double shape, double scale, double rate, unsigned int seed);
+    PC();
+    std::vector<std::shared_ptr<Port>> getPorts() const; // Declaration of getPorts()
+
+    explicit PC(int id); // Constructor that accepts an int
+
+    void requestIP(shared_ptr<Router> router);
 
     // Set known IPs of other users
     void setOtherIPs(const std::vector<std::string>& ips);
@@ -42,6 +51,12 @@ public:
 
     // Retransmit packets
     void retransmitPacket(int sequenceNumber, Router* gateway);
+
+    void addPort(std::shared_ptr<Port> port);
+
+    std::string getIpAddress() const;
+    std::string getMacaddress() const;
+    void setMacaddress(const std::string &newMacaddress);
 };
 
 #endif // PC_H
